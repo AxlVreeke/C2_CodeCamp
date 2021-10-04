0fdf2d83a44181f5d1901341b10804fed92ebf3b
RobinDijke <70898113+RobinDijke@users.noreply.github.com>
2021-10-04 12:20:40 +0200

    navbar

--- index.php
+++ index.php
@@ -8,16 +8,45 @@
     <title>Hello World!</title>
 </head>
 <head>
-    <nav class="navMenu">
-        <a href="#">Home</a>
-        <a href="#">Blog</a>
-        <a href="#">Work</a>
-        <a href="#">About</a>
-        <div class="dot"></div>
-    </nav>
+
 </head>
 <body>
     <p>Hello World!</p>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<nav id="navbar" class="">
+  <div class="nav-wrapper">
+    <!-- Navbar Logo -->
+    <div class="logo">
+      <!-- Logo Placeholder for Inlustration -->
+      <a href="#home"><i class="fa fa-angellist"></i> Logo</a>
+    </div>
+
+    <!-- Navbar Links -->
+    <ul id="menu">
+      <li><a href="#home">Home</a></li><!--
+   --><li><a href="#services">Services</a></li><!--
+   --><li><a href="#about">About</a></li><!--
+   --><li><a href="#contact">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<!-- Menu Icon -->
+<div class="menuIcon">
+  <span class="icon icon-bars"></span>
+  <span class="icon icon-bars overlay"></span>
+</div>
+
+
+<div class="overlay-menu">
+  <ul id="menu">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+</div>
 </body>
 <footer>
     <p></p>
