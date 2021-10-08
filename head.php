0c9ee4f60b14b0fe9c0908d744c83d2450f6719e
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2021-10-08 12:51:40 +0200

    structure

--- head.php
+++ head.php
@@ -6,5 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeCamp</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
 
 </head>
