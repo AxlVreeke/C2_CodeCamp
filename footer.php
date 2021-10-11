9b7a179a4ecf550bca6b2ea219ebf16a9107fee0
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-11 13:43:40 +0200

    added footer

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
-
 <footer>
-
+    <div class="contactinformatie">
+        <p>Bedrijfnaam: 4S</p>
+        <p>Adres: Julianastraat 69</p>
+        <p>Postcode: 5964BT SCHOONHOVEN</p>
+        <p>Telefoon: 2155569420</p>
+    </div>
+    <div class="footer-img">
+        <img src="img/facebook.png" alt="Facebook Logo">
+        <img src="img/instagram.png" alt="Instagram Logo">
+    </div>
 </footer>
