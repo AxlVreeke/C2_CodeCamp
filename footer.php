bf1dc610c91b4b47b5cd522d146f49f402dc79d6
JesseKind <d279759@edu.rocwb.nl>
2021-10-11 13:47:36 +0200

    footer html css

--- footer.php
+++ footer.php
@@ -1,12 +1,14 @@
 <footer>
-    <div class="contactinformatie">
-        <p>Bedrijfnaam: 4S</p>
-        <p>Adres: Julianastraat 69</p>
-        <p>Postcode: 5964BT SCHOONHOVEN</p>
-        <p>Telefoon: 2155569420</p>
-    </div>
-    <div class="footer-img">
-        <img src="img/facebook.png" alt="Facebook Logo">
-        <img src="img/instagram.png" alt="Instagram Logo">
+    <div class="footer_container">
+        <div class="contactinformatie">
+            <p>Bedrijfnaam: 4S</p>
+            <p>Adres: Julianastraat 69</p>
+            <p>Postcode: 5964BT SCHOONHOVEN</p>
+            <p>Telefoon: 2155569420</p>
+        </div>
+        <div class="footer-img">
+            <img src="img/facebook.png" alt="Facebook Logo">
+            <img src="img/instagram.png" alt="Instagram Logo">
+        </div>
     </div>
 </footer>
