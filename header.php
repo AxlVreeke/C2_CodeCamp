d70ee26bb6da73b596a7adefd16d18266cb086eb
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-11 12:44:36 +0200

    Header code

--- header.php
+++ header.php
@@ -1,3 +1,14 @@
 <header>
-
+  <nav>
+    <div class="header-container">
+    <div class="img-nav">
+      <img src="img/codecamplogo.png" alt="logo">
+    </div>
+    <div class="buttons-nav">
+      <a href="index.php">Home</a>
+      <a href="resources/services.php">Services</a>
+      <a href="resources/contact.php">Contact</a>
+      </div>
+    </div>
+  </nav>
 </header>
