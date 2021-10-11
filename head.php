2be9b89d2442f6babbfc43959efc966ebe1ed17f
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-11 13:37:25 +0200

    update head

--- head.php
+++ head.php
@@ -6,6 +6,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeCamp</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/header.css">
     <script src="JS/script.js"></script>
 </head>
