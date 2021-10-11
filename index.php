d7b204419289ae0ad3db6348434d33a830bbefec
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2021-10-11 13:25:41 +0200

    fix

--- index.php
+++ index.php
@@ -4,12 +4,13 @@
   <?php require_once'header.php' ?>
 </header>
 <body onload="startTime()">
-<div class="container">
-  <div class="live-time">
-    <p><?php  $today = date("F j"); echo $today; ?></p>
-    <div id="time"></div>
+  <div class="container">
+    <div class="live-time">
+      <p><?php  $today = date("F j"); echo $today; ?></p>
+      <div id="time"></div>
+    </div>
   </div>
-</div>
+</body>
 <footer>
 <?php require_once'footer.php' ?>
 </html>
