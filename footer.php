87ce2f3fad3c5e206c6b18b7aedeee763dc9b587
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-14 11:36:54 +0200

    Footer changes

--- footer.php
+++ footer.php
@@ -1,14 +1,26 @@
 <footer>
     <div class="footer_container">
+    <p style="text-align: center"> Copyright &copy; <script>document.write(new Date().getFullYear())</script> CodeCamp All Rights Reserved</p>
         <div class="contactinformatie">
+        
             <p>Bedrijfnaam: 4S</p>
             <p>Adres: Julianastraat 69</p>
             <p>Postcode: 5964BT SCHOONHOVEN</p>
             <p>Telefoon: 2155569420</p>
         </div>
         <div class="footer-img">
+            <a href="https://www.facebook.com" target="_blank">
             <img src="img/facebook.png" alt="Facebook Logo">
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
             <img src="img/instagram.png" alt="Instagram Logo">
+            </a>
+            <a href="https://www.youtube.com" target="_blank">
+            <img src="img/youtube.png" alt="youtube Logo">
+            </a>
+            <a href="https://twitter.com" target="_blank">
+            <img src="img/twitter.png" alt="twitter Logo">
+            </a>
         </div>
     </div>
 </footer>
