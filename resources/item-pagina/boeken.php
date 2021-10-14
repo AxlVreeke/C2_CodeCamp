8ceb98d46a139eb5affbba6b6739ac202f9ea6b7
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-14 11:57:34 +0200

    Revert "fixed all rescources"
    This reverts commit 4fcd278655cdc063a79a5943a6f81a6e27ea650e.

--- resources/item-pagina/boeken.php
+++ resources/item-pagina/boeken.php
@@ -10,47 +10,14 @@
   <nav>
     <div class="header-container">
     <div class="img-nav">
-      <img src="../../img/codecamplogo.png" class="logo-codecamp" alt="logo">
+      <a href="index.php">
+      <img src="../../img/codecamplogo.png" alt="logo" class="responsive">
+      </a>
     </div>
     <div class="buttons-nav">
-      <a href="index.php">
-    <button class="button_home" type="button">Home</button>
-    </a>
-      <div class="dropdown">
-    <button class="dropbtn">Songteksten
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="../../resources/song-pagina/bohemian-rhapsody.php">Bohemian rhapsody</a>
-      <a href="../../resources/song-pagina/killer-queen.php">Killer queen</a>
-      <a href="../../resources/song-pagina/madness.php">Madness</a>
-      <a href="../../resources/song-pagina/plug-in-baby.php">Plug in baby</a>
-      <a href="../../resources/song-pagina/transylvia.php">Transylvia</a>
-      <a href="../../resources/song-pagina/uprising.php">Uprising</a>
-      <a href="../../resources/song-pagina/we-are-the-champions.php">We are the champions</a>
-    </div>
-      </div>
-      <div class="dropdown">
-    <button class="dropbtn">Voorwerpen
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="../../resources/item-pagina/boeken.php">Boeken</a>
-      <a href="../../resources/item-pagina/cirkelzagen.php">Cirkel zagen</a>
-      <a href="../../resources/item-pagina/hamers.php">Hamers</a>
-      <a href="../../resources/item-pagina/kettingzagen.php">Kettingzagen</a>
-    </div>
-      </div>
-      <div class="dropdown">
-    <button class="dropbtn">Informatie Pagina
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="../../resources/info-pagina/darth-vader.php">Darth vader</a>
-      <a href="../../resources/info-pagina/james-bond.php">James Bond</a>
-      <a href="../../resources/info-pagina/luke-skywalker.php">Luke Skywalker</a>
-      <a href="../../resources/info-pagina/timoty-dalton.php">Timoty Dalton</a>
-    </div>
+      <a href="../../index.php">Home</a>
+      <a href="../resources/services.php">Services</a>
+      <a href="../resources/contact.php">Contact</a>
       </div>
     </div>
   </nav>
