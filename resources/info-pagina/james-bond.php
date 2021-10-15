62cb03635eca5f6142a8374ceb70d6afb1e89135
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-15 13:12:54 +0200

    james bond pagina toegevoegd

--- resources/info-pagina/james-bond.php
+++ resources/info-pagina/james-bond.php
@@ -23,7 +23,27 @@
   </nav>
 </header>
 <body>
-    <p>test</p>
+<div class="container">
+    <div class="text">
+      <div class="items">
+        <div class="item-text">
+          <h1>James Bond films met Roger Moore</h1>
+          <ul>
+            <li>Live and Let Die</li>
+            <li>The Man with the Golden Gun</li>
+            <li>The Spy Who Loved Me</li>
+            <li>Moonraker</li>
+            <li>For Your Eyes Only</li>
+            <li>Octopussy</li>
+            <li>A View to a Kill</li>
+          </ul>  
+        </div> 
+        <div class="img-boeken">
+          <img src="../../IMG/rogermoore.jpg" alt="Roger Moore" class="img-item">
+        </div> 
+      </div>
+    </div>
+  </div>
 </body>
 <footer>
     <div class="footer_container">
