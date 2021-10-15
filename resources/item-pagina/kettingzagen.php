754234bcb1ec9816c7a28cf87949428e8f2633fb
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-15 12:42:45 +0200

    kettingzaag pagina toegevoegd

--- resources/item-pagina/kettingzagen.php
+++ resources/item-pagina/kettingzagen.php
@@ -23,7 +23,22 @@
   </nav>
 </header>
 <body>
-    <p>test</p>
+<div class="container">
+    <div class="text">
+      <div class="items">
+        <div class="item-text">
+          <h1>Kettingzagen</h1>
+          <p>Een kettingzaag (ook motorzaag, motorkettingzaag of boomzaag genoemd) is een draagbare machine om mee te zagen. De belangrijkste functie is hout zagen.</p>
+          <p>Er zijn verschillende typen aandrijving voor kettingzagen: pneumatisch, hydraulisch, elektrisch en door middel van een verbrandingsmotor (vaak ook motorkettingzaag genoemd). De elektrische kettingzaag en de motorkettingzaag zijn het meest gangbaar.</p>
+          <p>Elektrische kettingzagen zijn met name bedoeld voor particulier gebruik. Ze zijn milieuvriendelijker en maken doorgaans minder lawaai dan kettingzagen op benzine. Ze zijn praktischer in gebruik doordat ze lichter zijn.</p>
+          <p>Elektrische kettingzagen zijn vaak goedkoper in aanschaf. Nadeel is het snoer bij sommige elektrische kettingzagen waardoor de actieradius beperkt is, maar draadloze elektrische kettingzagen bestaan ook. Deze werken op een accu.</p>
+        </div> 
+        <div class="img-boeken">
+          <img src="../../IMG/kettingzaag.png" alt="boeken" class="img-item">
+        </div> 
+      </div>
+    </div>
+  </div>
 </body>
 <footer>
     <div class="footer_container">
