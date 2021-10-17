eac4c51d7d8a26d3288bb573d2cb99f794761e89
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-17 22:53:16 +0200

    added all info pages

--- resources/info-pagina/george-clooney.php
+++ resources/info-pagina/george-clooney.php
@@ -1,3 +1,15 @@
 <?php require_once '../../header.php' ?>
 
+<div class="container">
+  <div class="text">
+    <h1>George Clooney</h1>
+    <p class="lyrics">
+        George Timothy Clooney (Lexington, Kentucky, 6 mei 1961) is een Amerikaans acteur, regisseur, scenarioschrijver en producent met meer dan dertig filmprijzen en nominaties op zijn naam. Voor de film Syriana won hij een Oscar voor beste mannelijke bijrol. Tevens won hij als producent een Oscar voor beste film voor Argo. Ook kreeg hij Oscarnominaties voor zijn rollen in Michael Clayton en The Descendants, een European Film Award (voor Good Night, and Good Luck) en meerdere Golden Globes (voor Syriana en O Brother, Where Art Thou?). <br>
+        Clooney is de zoon van televisiepersoonlijkheid Nick Clooney en Nina Bruce Warren. Hij heeft een oudere zus, Ada Zeidler. Zijn vader is de broer van zangeres-actrice Rosemary Clooney. <br>
+        Op jonge leeftijd leerde Clooney omgaan met de camera; zijn vader nam vaak zijn familie mee naar publieke optredens en de jonge Clooney verscheen zelfs in een van zijn televisieprogramma's. Op de Augusta High School was Clooney een begaafd honkballer, maar tijdens een tryout bij de Cincinnati Reds bleek hij niet goed genoeg om prof te worden.
+
+    </p>
+  </div>
+</div>
+
 <?php require_once '../../footer.php' ?>
