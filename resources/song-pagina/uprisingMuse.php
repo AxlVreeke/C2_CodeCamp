56068ee01003a64735500eca22a3086511cca940
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-17 22:02:25 +0200

    muse

--- resources/song-pagina/uprisingMuse.php
+++ resources/song-pagina/uprisingMuse.php
@@ -1,84 +1,46 @@
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CodeCamp</title>
-    <link rel="stylesheet" type="text/css" href="../../css/style.css">
-    <script src="JS/script.js"></script>
-</head>
-<header>
-  <nav>
-    <div class="header-container">
-    <div class="img-nav">
-      <a href="index.php">
-      <img src="../../img/codecamplogo.png" alt="logo" class="responsive">
-      </a>
-    </div>
-    <div class="buttons-nav">
-      <a href="../../index.php">Home</a>
-      <a href="../resources/services.php">Services</a>
-      <a href="../resources/contact.php">Contact</a>
-      </div>
-    </div>
-  </nav>
-</header>
-<body>
-<div class="container orange">
-      <div class="text">
-        <h1>Uprising ~Muse</h1>
-        <p class="lyrics">
-        Paranoia is in bloom
-The PR transmissions will resume
-They'll try to push drugs that keep us all dumbed down
-And hope that we will never see the truth around
-(So come on)
-Another promise, another scene
-Another packaged lie to keep us trapped in greed
-And all the green belts wrapped around our minds
-And endless red tape to keep the truth confined
-(So come on)
-They will not force us
-They will stop degrading us
-They will not control us
-We will be victorious
-(So come on)
-Interchanging mind control
-Come, let the revolution take its toll
-If you could flick the switch and open your third eye
-You'd see that we should never be afraid to die
-(So come on)
-Rise up and take the power back
-It's time the fat cats had a heart attack
-You know that their time's coming to an end
-We have to unify and watch our flag ascend
-(So come on)
-They will not force us
-They will stop degrading us
-They will not control us
-We will be victorious
-(So come on)
-They will not force us
-They will stop degrading us
-They will not control us
-We will be victorious
-(So come on) 
-      </p>
-    </div>
-  </div>
+<?php require_once '../../header.php' ?>
 
-</body>
-<footer>
-    <div class="footer_container">
-        <div class="contactinformatie">
-            <p>Bedrijfnaam: 4S</p>
-            <p>Adres: Julianastraat 69</p>
-            <p>Postcode: 5964BT SCHOONHOVEN</p>
-            <p>Telefoon: 2155569420</p>
-        </div>
-        <div class="footer-img">
-            <img src="../../img/facebook.png" alt="Facebook Logo">
-            <img src="../../img/instagram.png" alt="Instagram Logo">
-        </div>
-    </div>
-</footer>
-</html>
+<div class="container orange">
+  <div class="text">
+    <h1>Uprising ~Muse</h1>
+    <p class="lyrics">
+      Paranoia is in bloom
+      The PR transmissions will resume
+      They'll try to push drugs that keep us all dumbed down
+      And hope that we will never see the truth around
+      (So come on)
+      Another promise, another scene
+      Another packaged lie to keep us trapped in greed
+      And all the green belts wrapped around our minds
+      And endless red tape to keep the truth confined
+      (So come on)
+      They will not force us
+      They will stop degrading us
+      They will not control us
+      We will be victorious
+      (So come on)
+      Interchanging mind control
+      Come, let the revolution take its toll
+      If you could flick the switch and open your third eye
+      You'd see that we should never be afraid to die
+      (So come on)
+      Rise up and take the power back
+      It's time the fat cats had a heart attack
+      You know that their time's coming to an end
+      We have to unify and watch our flag ascend
+      (So come on)
+      They will not force us
+      They will stop degrading us
+      They will not control us
+      We will be victorious
+      (So come on)
+      They will not force us
+      They will stop degrading us
+      They will not control us
+      We will be victorious
+      (So come on)
+    </p>
+  </div>
+</div>
+
+<?php require_once '../../footer.php' ?>
