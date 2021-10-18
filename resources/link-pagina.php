9039c88bdfea8c66669bea39f9e95afd76459960
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2021-10-18 08:40:43 +0200

    update

--- resources/link-pagina.php
+++ resources/link-pagina.php
@@ -1,5 +1,11 @@
 <?php require_once '../header.php' ?>
     
-
+<div class="links">
+    <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+    </ul>
+</div>
 
 <?php require_once '../footer.php' ?>
