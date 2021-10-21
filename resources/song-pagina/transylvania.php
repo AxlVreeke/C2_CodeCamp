7a12fafd7ecd72a65df1e9503cf3d91ad2a6075d
Rooijke <70897974+Rooijke@users.noreply.github.com>
2021-10-21 11:34:07 +0200

    transylvania fix

---
+++ resources/song-pagina/transylvania.php
@@ -0,0 +1,11 @@
+<?php require_once '../../header.php' ?>
+
+<div class="container red">
+  <div class="text">
+    <h1>Iron Maiden ~Transylvania Lyrics</h1>
+    <p class="lyrics">
+      [instrumental]
+    </p>
+  </div>
+</div>
+<?php require_once '../../footer.php' ?>
