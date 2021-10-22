66611c6330c2778ade5f6830952c9f535586e436
AxlVreeke <D283491@edu.rocwb.nl>
2021-10-22 12:27:01 +0200

    update cola, up, sinas pagina's

--- resources/dranken-pagina/frisdrank/up.php
+++ resources/dranken-pagina/frisdrank/up.php
@@ -1,16 +1,25 @@
-<?php require_once '../../../header.php' ?>
+<?php require_once '../../../header.php'; ?>
 <div class="container orange">
-  <div class="text">
-    <h1>Up</h1>
-    <p class="lyrics">
-    7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld. <br>
-    1929: De originele 7UP, een kleurloze koolzuurhoudende frisdrank met lemon-limesmaak, werd bedacht door Charles Leiper Grigg uit St. Louis, Missouri in de Verenigde Staten.[1] De originele naam was Bib-Label Lithiated Lemon-Lime Soda, maar de naam werd snel veranderd in de huidige naam 7UP. <br>
-    1952: De introductie op de Nederlandse markt door Joe van Bussel en Gerrit van Rooij te Amsterdam onder de naam 'G.B. van Rooij N.V.', een naam die in 1953 werd gewijzigd in: 'Seven-Up Bottling Company Amsterdam N.V.' die weer productielicenties uitgaf aan regionale bottelarijen als Vrumona, dit om het product zo spoedig mogelijk op de markt te krijgen. <br>
-    1961: De Coca-Cola Company introduceert Sprite om de concurrentie aan te gaan met 7UP. <br>
-    1970: 7UP introduceert zijn eerste light variant onder de naam: Sugar-Free 7UP. <br>
-    1987: Cherry 7UP and Diet Cherry 7UP maken hun debuut in de markt.
-    </p>
+  <div class="item">
+    <div class="item-title">
+      <h1>Up</h1>
+    </div>
+    <div class="item-text">
+      <p>
+        7Up (of Seven-Up) is een frisdrankmerk van Dr Pepper Snapple Group in de Verenigde Staten en PepsiCo in de rest van de wereld.
+        Het merk werd sinds begin jaren 90 aangeprezen door Fido Dido, een getekende figuur met een relaxte uitstraling. Fido Dido maakte in 2019 zijn comeback.
+      </p>
+    </div>
+    <div class="item-text-2">
+      <p>
+        1929: De originele 7UP, een kleurloze koolzuurhoudende frisdrank met lemon-limesmaak, werd bedacht door Charles Leiper Grigg uit St. Louis, Missouri in de Verenigde Staten.[1] De originele naam was Bib-Label Lithiated Lemon-Lime Soda, maar de naam werd snel veranderd in de huidige naam 7UP.
+        1952: De introductie op de Nederlandse markt door Joe van Bussel en Gerrit van Rooij te Amsterdam onder de naam 'G.B. van Rooij N.V.', een naam die in 1953 werd gewijzigd in: 'Seven-Up Bottling Company Amsterdam N.V.' die weer productielicenties uitgaf aan regionale bottelarijen als Vrumona, dit om het product zo spoedig mogelijk op de markt te krijgen.
+        1961: De Coca-Cola Company introduceert Sprite om de concurrentie aan te gaan met 7UP.
+      </p>
+    </div>
+    <div class="img-boeken">
+      <img src="<?= $base_url ?>/IMG/7up.png" alt="boeken" class="img-item">
+    </div>
   </div>
 </div>
-
-<?php require_once '../../../footer.php' ?>
+<?php require_once "../../../footer.php"; ?>
