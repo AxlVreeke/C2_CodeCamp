0ce4e5fe2e7bf827db6d0f41dcb93f74adc34d27
AxlVreeke <D283491@edu.rocwb.nl>
2021-11-11 21:01:43 +0100

    Update edit.php

--- resources/admin-pagina/edit.php
+++ resources/admin-pagina/edit.php
@@ -12,13 +12,13 @@
     <label for="fname">Titel:</label>
     <input type="text" name="title" value="<?php echo $boek['title']; ?>">
     <label for="fname">Auteur:</label>
-    <input type="text" name="title" value="<?php echo $boek['author']; ?>">
+    <input type="text" name="author" value="<?php echo $boek['author']; ?>">
     <label for="fname">Prijs:</label>
-    <input type="number" name="title" value="<?php echo $boek['price']; ?>">
+    <input type="number" name="price" value="<?php echo $boek['price']; ?>">
     <label for="fname">Pagina's:</label>
-    <input type="number" name="title" value="<?php echo $boek['pages']; ?>">
+    <input type="number" name="pages" value="<?php echo $boek['pages']; ?>">
     <label for="fname">Beschrijving:</label>
-    <input type="text" name="title" value="<?php echo $boek['description']; ?>">
+    <textarea rows="4" cols="50" name="description" form="usrform"><?php echo $boek['description']; ?></textarea>
     <input type="submit" value="Opslaan">
 </form>
 
