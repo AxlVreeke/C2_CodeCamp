2e40bf3b0ec0a640f6572c87742a9493512ee9c9
AxlVreeke <D283491@edu.rocwb.nl>
2021-11-11 21:29:23 +0100

    update edit page

--- resources/admin-pagina/edit.php
+++ resources/admin-pagina/edit.php
@@ -8,19 +8,23 @@
     $statement->execute([":id" => $id]);
     $boek = $statement->fetch(PDO::FETCH_ASSOC);
 ?>
-<form action="">
-    <label for="fname">Titel:</label>
-    <input type="text" name="title" value="<?php echo $boek['title']; ?>">
-    <label for="fname">Auteur:</label>
-    <input type="text" name="author" value="<?php echo $boek['author']; ?>">
-    <label for="fname">Prijs:</label>
-    <input type="number" name="price" value="<?php echo $boek['price']; ?>">
-    <label for="fname">Pagina's:</label>
-    <input type="number" name="pages" value="<?php echo $boek['pages']; ?>">
-    <label for="fname">Beschrijving:</label>
-    <textarea rows="4" cols="50" name="description" form="usrform"><?php echo $boek['description']; ?></textarea>
-    <input type="submit" value="Opslaan">
-</form>
+<div class="edit-form">
+    <form action="">
+        <label for="fname">Titel:</label>
+        <input type="text" name="title" value="<?php echo $boek['title']; ?>">
+        <label for="fname">Auteur:</label>
+        <input type="text" name="author" value="<?php echo $boek['author']; ?>">
+        <label for="fname">Prijs:</label>
+        <input type="number" name="price" value="<?php echo $boek['price']; ?>">
+        <label for="fname">Pagina's:</label>
+        <input type="number" name="pages" value="<?php echo $boek['pages']; ?>">
+        <label for="fname">Beschrijving:</label>
+        <textarea rows="4" cols="50" name="description" form="usrform"><?php echo $boek['description']; ?></textarea>
+    </form>
+</div>
+<div class="opslaan">
+    <button><a href="../../">Opslaan</a></button>
+</div>
 
 
 <?php require_once "../../footer.php"; ?>
