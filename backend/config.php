c82e563ebdb1fe101d52a938e39dc009a83dd7c8
AxlVreeke <D283491@edu.rocwb.nl>
2021-11-11 11:29:50 +0100

    update database

--- backend/config.php
+++ backend/config.php
@@ -1,2 +1,16 @@
 <?php
+
+//IEDER VOOR ZICH:
+
+#### TODO
+//Hernoem dit bestand naar 'config.php' en vul jouw eigen database-gegevens in.
+//Deze config wordt hierna _niet_ meegestuurd naar je groepsgenoten. Zo kan iedereen zijn eigen wachtwoord, etc. invullen.
+
+$dbHost = 'localhost';
+$dbName = 'codecamp';
+$dbUser = 'root';
+$dbPass = '';
+
+//De url waarop jouw project staat. Géén slash aan het einde.
 $base_url = 'http://localhost/C2_CodeCamp';
+
